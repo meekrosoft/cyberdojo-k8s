5a5dcdce08a56400ef79f1aaaa6b743bd7ed68ac
Mike Long <mike@praqma.com>
2018-02-01 07:50:08 +0100

    Update create script with some deployments and services

--- create.sh
+++ create.sh
@@ -8,3 +8,11 @@ for file in *persistentvolumeclaim.yaml
 do
     kubectl $action -f $file
 done
+
+
+kubectl create -f starter-deployment.yaml
+kubectl create -f starter-service.yaml
+kubectl create -f web-deployment.yaml
+kubectl create -f web-service.yaml
+kubectl create -f nginx-deployment.yaml
+kubectl create -f nginx-pod.yaml
