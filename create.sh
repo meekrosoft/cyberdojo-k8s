5e6a7f3edbac6440ed5ff8c8f45bd1b63d7bb8e7
Mike Long <mike@praqma.com>
2018-04-27 14:07:33 +0200

    Refactor create script

--- create.sh
+++ create.sh
@@ -4,32 +4,17 @@ set -x
 
 action=create
 # persistentVolumeClaim
-for file in *persistentvolumeclaim.yaml
+for file in *pvc.yaml
 do
     echo NOT CREATING kubectl $action -f $file
 done
 
+for file in *deployment.yaml
+do
+    echo kubectl $action -f $file
+done
 
-kubectl create -f starter-deployment.yaml
-kubectl create -f starter-service.yaml
-kubectl create -f web-deployment.yaml
-kubectl create -f web-service.yaml
-kubectl create -f nginx-deployment.yaml
-kubectl create -f nginx-service.yaml
-kubectl create -f storer-data-pvc.yaml
-kubectl create -f storer-deployment.yaml
-kubectl create -f storer-service.yaml
-kubectl create -f runner-stateless-deployment.yaml
-kubectl create -f runner-stateless-service.yaml
-kubectl create -f differ-deployment.yaml
-kubectl create -f differ-service.yaml
-kubectl create -f zipper-deployment.yaml
-kubectl create -f zipper-service.yaml
-
-# Monitoring
-kubectl create -f prometheus-deployment.yaml
-kubectl create -f prometheus-service.yaml
-
-kubectl create -f grafana-data-pvc.yaml
-kubectl create -f grafana-deployment.yaml
-kubectl create -f grafana-service.yaml
+for file in *service.yaml
+do
+    echo kubectl $action -f $file
+done
